717bf0db9637dfb4a0377552b9abecccfb6d5a77
CadLuisPablo <cad.luispablo@gmail.com>
2022-10-03 06:52:21 +0200

    News Scripts

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -10,22 +10,25 @@ public class MainManager : MonoBehaviour
     public int LineCount = 6;
     public Rigidbody Ball;
 
+    public Text BestScoreText;
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        SetBestScore();
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -65,12 +68,30 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        ScoreText.text = $"Puntuación : {m_Points}";
     }
 
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+
+        if (m_Points > DataManager.Instance.BestScorePoints)
+        {
+            DataManager.Instance.BestScorePlayer = DataManager.Instance.PlayerName;
+            DataManager.Instance.BestScorePoints = m_Points;
+            BestScoreText.text = $"Mejor puntuación : {DataManager.Instance.BestScorePlayer} : {DataManager.Instance.BestScorePoints}";
+            DataManager.Instance.SaveBestScore();
+        }
+
+    }
+
+    public void SetBestScore()
+    {
+        if (DataManager.Instance != null)
+        {
+            DataManager.Instance.LoadBestScore();
+            BestScoreText.text = $"Mejor puntuación : {DataManager.Instance.BestScorePlayer} : {DataManager.Instance.BestScorePoints}";
+        }
     }
 }
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField NameInput;
+    [SerializeField] private TextMeshProUGUI TitleText;
+
+    private void Start()
+    {
+        TitleText.SetText(string.Format("Mejor puntuación : {0} : {1}", DataManager.Instance.BestScorePlayer, DataManager.Instance.BestScorePoints));
+        NameInput.text = DataManager.Instance.BestScorePlayer;
+    }
+
+    public void StartNew()
+    {
+        SetPlayerName();
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void SetPlayerName()
+    {
+        DataManager.Instance.PlayerName = NameInput.text;
+    }
+}
